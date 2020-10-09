c4bd7b9fb052d2dfe217d72dcc42b8a2d2d9e8dc
Jung Lee <lee.jung.s95@gmail.com>
2020-10-09 17:57:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,10 +18,10 @@ class Pokemon
 
   def self.find(id, db)
     new_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
-
+    binding.pry
     new_pokemon.id = new_pokemon[0][0]
     new_pokemon.name = new_pokemon[0][1]
     new_pokemon.type = new_pokemon[0][2]
-    binding.pry
+
   end
 end
